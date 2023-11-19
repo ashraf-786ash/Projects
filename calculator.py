5fb5d2ce24a4a13e0f4cf9df41d0e1d5a5a3d304
ashraf-786ash <151422014+ashraf-786ash@users.noreply.github.com>
2023-11-19 10:44:53 +0530

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,43 @@
+def add(x, y):
+    return x + y
+
+def subtract(x, y):
+    return x - y
+
+def multiply(x, y):
+    return x * y
+
+def divide(x, y):
+    if y != 0:
+        return x / y
+    else:
+        return "Cannot divide by zero."
+
+def get_user_input():
+    while True:
+        try:
+            num1 = float(input("Enter the first number: "))
+            num2 = float(input("Enter the second number: "))
+            operation = input("Enter the operation (+, -, *, /): ")
+
+            return num1, num2, operation
+        except ValueError:
+            print("Invalid input. Please enter valid numbers.")
+
+if __name__ == "__main__":
+    print("Welcome to the Simple Calculator!")
+
+    num1, num2, operation = get_user_input()
+
+    if operation == '+':
+        result = add(num1, num2)
+    elif operation == '-':
+        result = subtract(num1, num2)
+    elif operation == '*':
+        result = multiply(num1, num2)
+    elif operation == '/':
+        result = divide(num1, num2)
+    else:
+        result = "Invalid operation. Please enter +, -, *, or /."
+
+    print(f"\nResult: {result}")
